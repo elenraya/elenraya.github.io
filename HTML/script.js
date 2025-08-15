f9ca32433e5432e27d05ced4defa6d6a4bbcd8a4
elenraya <elen.macario.raya@gmail.com>
2025-08-15 20:33:46 -0300

    UP

---
+++ HTML/script.js
@@ -0,0 +1,10 @@
+function openNav(){
+    let x = document.getElementById('navigation');
+    if (x.className === 'navigation'){
+        x.className += " menujs";
+        document.getElementById('treeline-icon').innerHTML = "&Cross"
+    }else{
+        x.className = 'navigation'
+        document.getElementById('treeline-icon').innerHTML = "&#9776;";
+    }
+}
